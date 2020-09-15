df35f6195cbf76fe22173a15ed3165f11db4f97e
Florian <herzog-florian@gmx.de>
2020-09-15 11:23:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-echo "==> Fetching argocd cli..."
-curl -sSL -o /usr/local/bin/argocd https://${ARGOCD_CLI_SERVER}/download/argocd-linux-amd64
-chmod +x /usr/local/bin/argocd
-
 echo "==> Syncing app..."
-argocd app sync $1
+argocd app sync $1 --insecure
 
-echo "==> Waiting for app to become healthy..."
-argocd app wait $1
-
-echo "==> Generating app env url output..."
-env_url=$(argocd app get $1 -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
-echo "::set-output name=env_url::${env_url}"
+echo "==> Waiting ${ARGOCD_WAIT_TIMEOUT} seconds for app to become healthy..."
+argocd app wait $1 --timeout ${ARGOCD_WAIT_TIMEOUT} --insecure
